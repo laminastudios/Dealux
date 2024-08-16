b9492bd4f213696c6bf6df53e7cdc9b3f872fbed
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-08-16 20:29:53 +0800

    Added the database backbone for the User Group

---
+++ database/migrations/2024_08_16_121558_create_user_information.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_information', function (Blueprint $table) {
+            $table->id('user_info_id'); // Primary Key
+            $table->char('user_id', 12)->unique()->notNullable(); // Foreign Key reference from the User Table
+            $table->foreign('user_id')->references('user_id')->on('user_account')->onDelete('cascade');
+            $table->string('first_name', 20)->notNullable();
+            $table->string('mid_name', 20)->nullable();
+            $table->string('last_name', 20)->notNullable();
+            $table->char('gender', 1)->notNullable()->comment('M = Male, F = Female, P = Prefer not to say, O = Others');
+            $table->date('birth_date')->notNullable();
+            $table->char('phone_number', 11)->notNullable();
+            $table->char('subs_status', 1)->notNullable()->comment('B = Basic, P = Premium');
+            $table->timestamps(); // This will automatically handle created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_information');
+    }
+};
---
+++ database/migrations/2024_08_16_122801_create_payment_information.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_information', function (Blueprint $table) {
+            $table->id('payment_info_id'); // Primary Key
+            $table->char('user_id', 12)->notNullable(); // Foreign Key reference
+            $table->foreign('user_id')->references('user_id')->on('user_account')->onDelete('cascade');
+            $table->string('delivery_address', 100)->notNullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_information');
+    }
+};
