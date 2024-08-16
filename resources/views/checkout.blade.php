6be2a19b30a8869e7b0b921ffdd198518c6a3ca4
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-08-16 12:59:20 +0800

    Enclosed in HTML

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,5 +1,15 @@
-<div id="app">
-    <checkout-page></checkout-page>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dealux</title>
+</head>
+<body>
+    <div id="app">
+        <checkout-page></checkout-page>
+    </div>
 
-@vite('resources/js/app.js')
+    @vite('resources/js/app.js')
+</body>
+</html>
