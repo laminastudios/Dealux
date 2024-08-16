012e0893e1ef33bf5523b9d37a9ad32850894a67
Railey Desktop <raileyvictuelles@gmail.com>
2024-08-16 15:21:28 +0800

    feat: added `tailwindcss` - Installed Dev Dependencies:     - `tailwindcss`     - `postcss`     - `autoprefixer` - Added `postcss.config.js` and `tailwind.config.js` - Pointed directory:     - `./resources/**/*.vue` (any `.vue` file inside resources) - Connected all `blade.php` files to use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
