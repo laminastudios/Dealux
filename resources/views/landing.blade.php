c6d0f7080885eef372b397c8026141eb33e7ed89
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-08-18 17:33:12 +0800

    made the blade files a section

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dealux</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <div id="app">
-        <landing-page></landing-page>
-    </div>
+@extends('layouts.app')
+@section('content')
 
-    @vite('resources/js/app.js')
-</body>
-</html>
+<div id="app">
+    <landing-page></landing-page>
+</div>
+
+@endsection
+
