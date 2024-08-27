9540e9cb069e790496deff1667ef3c7cfc051f73
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-08-27 16:21:05 +0800

    fix: added v-bind so that it allows dynamic classes

--- resources/js/components/ui/Input.vue
+++ resources/js/components/ui/Input.vue
@@ -4,16 +4,17 @@
 <template>
     <div class="relative">
         <input
-            class="h-[48px] font-medium label-3"
+            class="h-[48px] block px-[24px] py-[13px] font-medium label-3 text-neutral-700 bg-neutral-50 border-0 border-b-2 border-neutral-50 appearance-none focus:outline-none focus:ring-0 focus:border-b-2 focus:border-neutral-600 peer"
+            v-bind="$attrs"
             :type="type"
             :id="id"
             :aria-label="label"
             :placeholder="''"
-            :class="inputClasses"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
         />
         <label
+            class="absolute label-3 text-neutral-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-[24px] peer-focus:dark:text-neutral-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
             :for="id"
             :class="labelClasses"
         >
@@ -41,16 +42,6 @@ export default {
         id: {
             type: String,
             default: 'floating_filled',
-        },
-        inputClasses: {
-            type: String,
-            default:
-                'block px-[24px] py-[13px] text-neutral-700 bg-neutral-50 border-0 border-b-2 border-neutral-50 appearance-none focus:outline-none focus:ring-0 focus:border-b-2  focus:border-neutral-600 peer',
-        },
-        labelClasses: {
-            type: String,
-            default:
-                'absolute label-3 text-neutral-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-[24px] peer-focus:dark:text-neutral-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto',
         },
     },
 };
