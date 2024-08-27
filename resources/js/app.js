d489f42d12bc2ed778ce5f9dc907732f98725317
Railey Desktop <raileyvictuelles@gmail.com>
2024-08-27 17:23:44 +0800

    feat: initial navbar setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,67 +1,65 @@
 // Importing Vue
-import { createApp } from "vue";
+import { createApp } from 'vue';
 
 // Importing the Vue-router
-import router from "./router";
+import router from './router';
 
 // Importing pages
-import LandingPage from "./pages/LandingPage.vue";
-import SignUpPage from "./pages/SignUpPage.vue";
-import SignInPage from "./pages/SignInPage.vue";
-import CartPage from "./pages/CartPage.vue";
-import ActiveOrderPage from "./pages/ActiveOrderPage.vue";
-import PurchaseHistoryPage from "./pages/PurchaseHistoryPage.vue";
-import SupportPage from "./pages/SupportPage.vue";
-import SubscriptionPage from "./pages/SubscriptionPage.vue";
-import ProfilePage from "./pages/ProfilePage.vue";
-import CheckoutPage from "./pages/CheckoutPage.vue";
-import HomePage from "./pages/HomePage.vue";
-import AboutPage from "./pages/AboutPage.vue";
-import SearchProductPage from "./pages/SearchProductPage.vue";
-import ProductPage from "./pages/ProductPage.vue";
-import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
-import RegisterInformationPage from "./pages/RegisterInformationPage.vue";
+import LandingPage from './pages/LandingPage.vue';
+import SignUpPage from './pages/SignUpPage.vue';
+import SignInPage from './pages/SignInPage.vue';
+import CartPage from './pages/CartPage.vue';
+import ActiveOrderPage from './pages/ActiveOrderPage.vue';
+import PurchaseHistoryPage from './pages/PurchaseHistoryPage.vue';
+import SupportPage from './pages/SupportPage.vue';
+import SubscriptionPage from './pages/SubscriptionPage.vue';
+import ProfilePage from './pages/ProfilePage.vue';
+import CheckoutPage from './pages/CheckoutPage.vue';
+import HomePage from './pages/HomePage.vue';
+import AboutPage from './pages/AboutPage.vue';
+import SearchProductPage from './pages/SearchProductPage.vue';
+import ProductPage from './pages/ProductPage.vue';
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+import RegisterInformationPage from './pages/RegisterInformationPage.vue';
 
 // Creating a new Vue application instance
 const app = createApp({});
 
 // Registering pages globally
-app.component("landing-page", LandingPage); // Registers LandingPage component
+app.component('landing-page', LandingPage); // Registers LandingPage component
 
-app.component("signup-page", SignUpPage); // Registers SignUpPage component
+app.component('signup-page', SignUpPage); // Registers SignUpPage component
 
-app.component("signin-page", SignInPage); // Registers SignInPage component
+app.component('signin-page', SignInPage); // Registers SignInPage component
 
-app.component("cart-page", CartPage); // Registers CartPage component
+app.component('cart-page', CartPage); // Registers CartPage component
 
-app.component("checkout-page", CheckoutPage); // Registers CheckoutPage component
+app.component('checkout-page', CheckoutPage); // Registers CheckoutPage component
 
-app.component("activeorder-page", ActiveOrderPage); // Registers ActiveOrderPage component
-app.component("purchasehistory-page", PurchaseHistoryPage); // Registers PurchaseHistoryPage component
+app.component('activeorder-page', ActiveOrderPage); // Registers ActiveOrderPage component
+app.component('purchasehistory-page', PurchaseHistoryPage); // Registers PurchaseHistoryPage component
 
-app.component("support-page", SupportPage); // Registers SupportPage component
-app.component("subscription-page", SubscriptionPage); // Registers SubscriptionPage component
-app.component("profile-page", ProfilePage); // Registers ProfilePage component
+app.component('support-page', SupportPage); // Registers SupportPage component
+app.component('subscription-page', SubscriptionPage); // Registers SubscriptionPage component
+app.component('profile-page', ProfilePage); // Registers ProfilePage component
 
-app.component("home-page", HomePage); // Registers HomePage component
+app.component('home-page', HomePage); // Registers HomePage component
 
-app.component("about-page", AboutPage); // Registers AboutPage component
+app.component('about-page', AboutPage); // Registers AboutPage component
 
-app.component("searchproduct-page", SearchProductPage); // Registers SearchProductPage component
+app.component('searchproduct-page', SearchProductPage); // Registers SearchProductPage component
 
-app.component("product-page", ProductPage); // Registers Product component
+app.component('product-page', ProductPage); // Registers Product component
 
-app.component("registerinfo-page", RegisterInformationPage); // Registers Register User Information component
-
+app.component('registerinfo-page', RegisterInformationPage); // Registers Register User Information component
 
 // Register the individual component here
-app.component("navbar", Navbar);
-app.component("footerbar", Footer);
-
+app.component('Navbar', Navbar);
+app.component('footerbar', Footer);
 
 // Using the Vue Router
 app.use(router);
 
 // Mounting the Vue application to the DOM element with id="app"
-app.mount("#app");
+app.mount('#app');
--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,45 +1,67 @@
 <!-- resources/js/components/Navbar.vue -->
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <router-link to="/" class="navbar-brand">This is just a tester navbar</router-link>
-      <ul class="navbar-nav">
-        <li class="nav-item"><router-link to="/">Nav1</router-link></li>
-        <li class="nav-item"><router-link to="/">Nav2</router-link></li>
-        <li class="nav-item"><router-link to="/">Nav3</router-link></li>
-        <li class="nav-item"><router-link to="/">Nav4</router-link></li>
-      </ul>
-    </div>
-  </nav>
+    <nav class="h-[60px] bg-neutral-300">
+        <div
+            v-if="isAuthenticated"
+            class="container border border-red-500 h-full flex flex-row"
+        >
+            <div class="my-auto">
+                <router-link to="/home">
+                    <Button>Logo</Button>
+                </router-link>
+            </div>
+            <ul class="m-auto flex flex-row gap-[76px]">
+                <li class="label-3 font-bold">
+                    <router-link to="/home">Home</router-link>
+                </li>
+                <li class="label-3 font-bold">
+                    <router-link to="/cart">My Cart</router-link>
+                </li>
+                <li class="label-3 font-bold">
+                    <router-link to="#">My Purchase</router-link>
+                </li>
+                <li class="label-3 font-bold">
+                    <router-link to="/support">Support Center</router-link>
+                </li>
+            </ul>
+            <div class="my-auto">
+                <button>Username</button>
+            </div>
+        </div>
+        <div
+            v-else
+            class="container border border-red-500 h-full flex justify-between"
+        >
+            <div class="my-auto">
+                <button>Logo</button>
+            </div>
+            <div class="flex gap-[8px]">
+                <Button
+                    variant="outline"
+                    class="my-auto"
+                >
+                    <router-link to="/register">Sign Up</router-link>
+                </Button>
+                <Button class="my-auto">
+                    <router-link to="/login">Sign In</router-link>
+                </Button>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
+import Button from './ui/Button.vue';
 export default {
-  name: 'Navbar',
+    name: 'Navbar',
+    props: {
+        isAuthenticated: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    components: {
+        Button,
+    },
 };
 </script>
-
-<style scoped>
-/* Add your CSS styling here */
-.navbar {
-  background-color: #333;
-  color: #fff;
-  padding: 1rem;
-}
-.navbar .container {
-  display: flex;
-  justify-content: space-between;
-}
-.navbar-nav {
-  list-style: none;
-  padding: 0;
-}
-.nav-item {
-  display: inline;
-  margin-left: 1rem;
-}
-.nav-item a {
-  color: #fff;
-  text-decoration: none;
-}
-</style>
