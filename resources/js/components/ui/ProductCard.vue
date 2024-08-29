d5a5ece526a9e75e6a5b57eb9b61c86dcba7f729
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-08-29 19:52:06 +0800

    feat: added product-card reusable component

---
+++ resources/js/components/ui/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col items-center">
+        <img
+            :src="product.image"
+            alt="Product Image"
+            class="h-[140px] w-full object-cover"
+        />
+        <div class="bg-neutral-100 p-[12px] w-full">
+            <h6>{{ product.name }}</h6>
+            <h6 class="text-red-500">{{ product.price }}</h6>
+            <p class="label-4 text-neutral-300">
+                <s>{{ product.oldPrice }}</s>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
