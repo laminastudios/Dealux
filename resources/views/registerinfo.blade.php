5488a407c3458d687b6c5de5c33e8b811e257bbc
jovieletteorlanda20 <157974031+jovieletteorlanda20@users.noreply.github.com>
2024-08-30 09:56:32 +0800

    Update registerinfo.blade.php

--- resources/views/registerinfo.blade.php
+++ resources/views/registerinfo.blade.php
@@ -1,9 +1,78 @@
 @extends('layouts.app')
+
 @section('content')
 
 <div id="app">
-    <registerinfo-page></registerinfo-page>
+    <registerinfo-page>
+        <form action="{{ route('register') }}" method="POST">
+            
+            
+           
+            <div class="form-group">
+                <label for="user_id">User ID</label>
+                <input type="text" id="user_id" name="user_id" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" id="first_name" name="first_name" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="middle_name">Middle Name</label>
+                <input type="text" id="middle_name" name="middle_name" class="form-control">
+            </div>
+
+            
+            <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" id="last_name" name="last_name" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="gender">Gender</label>
+                <select id="gender" name="gender" class="form-control" required>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="birth_date">Birth Date</label>
+                <input type="date" id="birth_date" name="birth_date" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="phone_number">Phone Number</label>
+                <input type="tel" id="phone_number" name="phone_number" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" class="form-control" required>
+            </div>
+
+            
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Register</button>
+            </div>
+        </form>
+    </registerinfo-page>
 </div>
 
 @endsection
+
   
