47a6314d7e003b9a015cb92468780ed3e19beb2e
Datsudo <76833632+datsudo@users.noreply.github.com>
2024-08-31 18:16:10 +0800

    add section for deleting/selecting all items

--- resources/js/pages/CartPage.vue
+++ resources/js/pages/CartPage.vue
@@ -2,6 +2,23 @@
     <section class="min-h-screen mx-auto">
         <div class="flex gap-5 justify-center container my-10">
             <div class="flex-1 h-full flex flex-col gap-5">
+                <div class="bg-neutral-100 px-4 py-2 flex justify-between">
+                    <div class="flex items-center gap-2 text-sm">
+                        <input
+                            type="checkbox"
+                            class="accent-neutral-300 bg-neutral-300"
+                        />
+                        <span class="font-medium leading-4">
+                            Select All (3 (items))
+                            <!-- TODO: compute total number of items -->
+                        </span>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <i class="bx bx-trash font-medium"></i>
+                        <span class="font-medium leading-4">Delete</span>
+                    </div>
+                </div>
+
                 <StoreCartItems
                     storeName="Dealux Store"
                     :items="[
