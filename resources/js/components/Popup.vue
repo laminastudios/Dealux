f78600c2971553fba4c3adb9966aede710531a3d
jovieletteorlanda20 <157974031+jovieletteorlanda20@users.noreply.github.com>
2024-09-02 10:21:20 +0800

    Create Popup.vue
    This component will handle the pop-up functionality for the registration form.

---
+++ resources/js/components/Popup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="popup" v-if="visible">
+    <div class="popup-content">
+      <span class="close" @click="closePopup">&times;</span>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    closePopup() {
+      this.visible = false;
+    },
+  },
+};
+</script>
