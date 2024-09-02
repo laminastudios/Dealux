592dad187bccf79423ed618db729ce8c9a46a613
jovieletteorlanda20 <157974031+jovieletteorlanda20@users.noreply.github.com>
2024-09-02 10:16:03 +0800

    Create TextInput.vue
    This component will be used for text-based inputs like user name, first name, middle name, last name, and phone number.

---
+++ resources/js/components/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-input">
+    <label>{{ label }}</label>
+    <input type="text" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    modelValue: String,
+  },
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+};
+</script>
