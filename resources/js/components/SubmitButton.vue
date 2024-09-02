c708479e296396f539fb71c35a330210b103511c
jovieletteorlanda20 <157974031+jovieletteorlanda20@users.noreply.github.com>
2024-09-02 10:20:12 +0800

    Create SubmitButton.vue
    This component will be used for the form submission button for the user registration.

---
+++ resources/js/components/SubmitButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="submit" class="submit-button">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Submit',
+    },
+  },
+};
+</script>
