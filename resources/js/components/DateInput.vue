9b9a40439a23c956216e714158942c00c911527c
jovieletteorlanda20 <157974031+jovieletteorlanda20@users.noreply.github.com>
2024-09-02 10:18:41 +0800

    Create DateInput.vue
    This component will be used for the birth date input.

---
+++ resources/js/components/DateInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="date-input">
+    <label>{{ label }}</label>
+    <input type="date" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    modelValue: String,
+  },
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+};
+</script>
