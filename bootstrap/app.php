257a2706d7244a03bff0d62b9d6b82b20f5a7457
Jose Ezra Nazarene Vergabera <120149324+ezravergabera@users.noreply.github.com>
2024-09-04 04:00:57 +0800

    refactor: removed the guard in auth.php and used the middleware in app.php instead

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'apiToken' => \App\Http\Middleware\HasApiToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
