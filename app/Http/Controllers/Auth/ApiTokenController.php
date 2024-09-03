5c44ff42b8c63c3c5ef7eb97da6fe600d610b7cd
Jose Ezra Nazarene Vergabera <120149324+ezravergabera@users.noreply.github.com>
2024-09-04 04:02:04 +0800

    feat: created a new method "getUser" for api routes to use

--- app/Http/Controllers/Auth/ApiTokenController.php
+++ app/Http/Controllers/Auth/ApiTokenController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\ApiTokensList;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class ApiTokenController extends Controller
 {
-    public function createApiToken(string $hashedToken)
+    public function createApiToken(string $user_id)
     {
         // check if the user is premium or not. number of uses will be -1 if premium. default is 3
         $no_of_uses = 3;
 
+        $api_token = Str::random(64);
+        $hashedToken = hash('sha256', $api_token);
+
         ApiTokensList::Create([
+            'user_id' => $user_id,
             'api_token' => $hashedToken,
             'number_of_uses' => $no_of_uses,
             'created_at' => now(),
@@ -33,5 +38,14 @@ class ApiTokenController extends Controller
         ])->save();
 
         return ['token' => $new_api_token];
+    }
+
+    public function getUser($token)
+    {
+        $apiToken = ApiTokensList::where('api_token', $token)->first();
+
+        $user = User::find($apiToken->user_id);
+
+        return $user;
     }
 }
