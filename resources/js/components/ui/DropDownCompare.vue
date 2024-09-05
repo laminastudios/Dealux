73d19234a3609cabfe885ece4db5f0c2bfec4095
Pilner <raileyvictuelles@gmail.com>
2024-09-06 00:40:23 +0800

    feat: modified DropDownCompare to have variants and dynamic styling

--- resources/js/components/ui/DropDownCompare.vue
+++ resources/js/components/ui/DropDownCompare.vue
@@ -3,7 +3,8 @@
         <button
             ref="dropDownButton"
             @click="openDropDown"
-            class="flex items-center justify-between gap-[10px] cursor-pointer w-full h-full transition-colors px-[33px] py-[7px] text-black bg-blue-100 hover:bg-blue-200 rounded-[5px]"
+            class="flex items-center justify-between gap-[10px] cursor-pointer w-full h-full transition-colors rounded-[5px]"
+            :class="variantClassesContainer"
         >
             <slot>{{ selected }}</slot>
         </button>
@@ -13,7 +14,7 @@
                 class="absolute top-full translate-x-1/2 right-1/2 text-center min-w-full"
                 :class="variantClassesDropDown"
             >
-                <div class="h-[33px] w-full bg-blue-300 flex items-center justify-center">
+                <div class="h-[33px] w-full flex items-center justify-center">
                     <p class="label-3 font-semibold text-white">{{ items.length }} Items Found</p>
                 </div>
                 <ul class="flex flex-col bg-blue-100 label-3 font-semibold">
@@ -22,7 +23,8 @@
                         :key="index"
                         href="#"
                         @click.prevent="selectOption(item)"
-                        class="transition-colors p-2 whitespace-nowrap cursor-pointer hover:bg-blue-200 py-[15px]"
+                        class="transition-colors p-2 whitespace-nowrap cursor-pointer py-[15px]"
+                        :class="variantClassesItems"
                     >
                         {{ item.Name }}
                     </li>
@@ -45,6 +47,7 @@ export default {
     methods: {
         openDropDown() {
             this.open = !this.open;
+            console.log(childClassContainer);
         },
         closeDropDown(event) {
             if (!this.$refs.dropDownButton.contains(event.target)) {
@@ -63,9 +66,47 @@ export default {
         document.removeEventListener('click', this.closeDropDown);
     },
     props: {
+        variant: {
+            type: String,
+            default: 'compare',
+        },
         items: {
             type: Array,
             required: true,
+        },
+        childClassContainer: {
+            type: String,
+            required: false,
+        },
+    },
+    computed: {
+        variantClassesContainer() {
+            const variants = {
+                variant1: 'bg-yellow-400 hover:bg-yellow-500 text-white',
+                variant2: 'bg-neutral-50 hover:bg-neutral-100 text-neutral-400',
+                navbar: 'hover:text-yellow-400',
+                compare: 'text-black bg-blue-100 hover:bg-blue-200',
+            };
+            let style = `${variants[this.variant]} ${this.childClassContainer}`;
+            return style || variants['light'];
+        },
+        variantClassesDropDown() {
+            const variants = {
+                variant1: 'bg-blue-300 text-primary-600 min-w-full label-4 border border-yellow-200 text-black',
+                variant2: 'bg-blue-300 text-primary-600 min-w-full label-4 border border-neutral-100 text-black',
+                navbar: 'bg-primary-700 text-black min-w-full label-3 font-semibold',
+                compare: 'bg-blue-300 text-black',
+            };
+            return variants[this.variant] || variants['light'];
+        },
+        variantClassesItems() {
+            const variants = {
+                variant1: 'hover:bg-blue-200',
+                variant2: 'hover:bg-blue-200',
+                navbar: 'hover:bg-blue-200',
+                compare: 'hover:bg-blue-200 ',
+            };
+            return variants[this.variant] || variants['light'];
         },
     },
 };
