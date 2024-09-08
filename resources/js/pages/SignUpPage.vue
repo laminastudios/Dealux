ad7c6e2f3b734589b1d6311989ecc41b7f85e62e
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-09-08 21:14:43 +0800

    Reset specific file to match dev branch

--- resources/js/pages/SignUpPage.vue
+++ resources/js/pages/SignUpPage.vue
@@ -3,18 +3,12 @@
 <template>
     <section class="min-h-screen flex items-center justify-center bg-gray-100">
         <div class="w-[1216px] h-[739px] bg-white shadow flex">
+            
             <!-- Left Side Container -->
             <div class="flex-1 flex flex-col items-center p-6">
-                <img
-                    src="assets/dealuxcircle.png"
-                    class="w-[80px] mt-[70px]"
-                    alt="Dealux Logo"
-                />
+                <img src="assets/dealuxcircle.png" class="w-[80px] mt-[70px]" alt="Dealux Logo">
                 <h2 class="text-2xl font-semibold text-center mb-6 mt-[21px]">Create Account</h2>
-                <form
-                    @submit.prevent="signUp"
-                    class="space-y-6 flex flex-col items-center mt-7"
-                >
+                <form @submit.prevent="signUp" class="space-y-6 flex flex-col items-center mt-7">
                     <float-input
                         v-model="userName"
                         type="text"
@@ -29,7 +23,7 @@
                         class="w-[465px] bg-neutral-50 border-neutral-50 custom-shadow"
                         required
                     />
-
+                    
                     <!-- Password field with toggleable eye icon -->
                     <div class="relative w-[465px]">
                         <float-input
@@ -39,31 +33,16 @@
                             class="w-full bg-neutral-50 border-neutral-50 custom-shadow"
                             required
                         />
-                        <span
-                            @click="togglePasswordVisibility"
-                            class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-                        >
-                            <img
-                                :src="showPassword ? 'assets/eye-off.png' : 'assets/eye.png'"
-                                class="h-6 w-6"
-                                alt="Toggle Password Visibility"
-                            />
+                        <span @click="togglePasswordVisibility" class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+                            <img :src="showPassword ? 'assets/eye-off.png' : 'assets/eye.png'" class="h-6 w-6" alt="Toggle Password Visibility" />
                         </span>
                     </div>
 
                     <div class="text-center label-4 text-black mt-8 font-semibold">
                         By signing up for an account, you agree to <br />
-                        <a
-                            href="#"
-                            class="text-black hover:underline"
-                            >Dealux’s Terms of Service</a
-                        >
-                        and
-                        <a
-                            href="#"
-                            class="text-black hover:underline"
-                            >Privacy Policy</a
-                        >.
+                        <a href="#" class="text-black hover:underline">Dealux’s Terms of Service</a> 
+                        and 
+                        <a href="#" class="text-black hover:underline">Privacy Policy</a>.
                     </div>
                     <div class="flex flex-col items-center">
                         <custom-button
@@ -75,10 +54,7 @@
                         >
                             <p class="label-3 font-semibold text-white">Sign Up</p>
                         </custom-button>
-                        <p
-                            v-if="errorMessage"
-                            class="text-red-500 text-center mt-4"
-                        >
+                        <p v-if="errorMessage" class="text-red-500 text-center mt-4">
                             {{ errorMessage }}
                         </p>
                     </div>
@@ -87,11 +63,7 @@
 
             <!-- Right Side Container -->
             <div class="relative w-[504px] h-full bg-primary-800 flex items-center justify-center flex-col">
-                <img
-                    src="assets/shapes.png"
-                    class="absolute inset-0 w-full h-full object-cover z-0"
-                    alt="Shapes"
-                />
+                <img src="assets/shapes.png" class="absolute inset-0 w-full h-full object-cover z-0" alt="Shapes">
                 <div class="relative z-10 text-center">
                     <h2 class="font-mainfont font-semibold text-white">Already have <br />an account?</h2>
                     <p class="label-4 text-white mt-8">Stay connected with the best deals!</p>
@@ -150,7 +122,7 @@ export default {
         },
         togglePasswordVisibility() {
             this.showPassword = !this.showPassword;
-        },
+        }
     },
 };
 </script>
